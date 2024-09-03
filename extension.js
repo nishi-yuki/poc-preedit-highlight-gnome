b1aa230247aa3f43d8cf4ab4ccef60dcd7e2f52f
Yuki Nishimura <nishiyk256@gmail.com>
2024-09-04 01:10:19 +0900

    Initial Commit

---
+++ extension.js
@@ -0,0 +1,97 @@
+/* extension.js
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
+import IBus from 'gi://IBus';
+
+import {Extension, InjectionManager} from 'resource:///org/gnome/shell/extensions/extension.js';
+
+import * as Main from 'resource:///org/gnome/shell/ui/main.js';
+import {InputMethod} from 'resource:///org/gnome/shell/misc/inputMethod.js';
+
+export default class ExampleExtension extends Extension {
+    enable() {
+        this._encoder = new TextEncoder();
+        this._injectionManager = new InjectionManager();
+        this._preeditVisible = false;
+        this._inputContext = null;
+
+        this._injectionManager.overrideMethod(
+            InputMethod.prototype,
+            'set_preedit_text',
+            originalMethod => {
+                return function (preedit, cursor, anchor, mode, callOriginal) {
+                    if (preedit === null && cursor === 0 && anchor === 0)  // on focus out
+                        originalMethod.call(this, null, 0, 0, mode);
+
+                    if (callOriginal === true) {
+                        console.log('[INPUT]', preedit, cursor, anchor);
+                        originalMethod.call(this, preedit, cursor, anchor, mode);
+                    }
+                };
+            }
+        );
+
+        this._onFocusWindow();
+        this._onFocusWindowID = global.display.connect(
+            'notify::focus-window',
+            this._onFocusWindow.bind(this)
+        );
+    }
+
+    disable() {
+        this._encoder = null;
+        this._injectionManager.clear();
+        this._injectionManager = null;
+        global.display.disconnect(this._onFocusWindowID);
+        this._inputContext?.disconnect(this._updatePreeditTextWithModeID);
+    }
+
+    _onFocusWindow() {
+        if (this._inputContext === Main.inputMethod._context)
+            return;
+        this._inputContext = Main.inputMethod._context;
+        this._updatePreeditTextWithModeID = this._inputContext.connect(
+            'update-preedit-text-with-mode',
+            (_con, text, pos, visible, mode) => {
+                let s = text.get_text();
+                let attrs = text.get_attributes();
+                let attr;
+                let end = pos;
+
+                for (let i = 0; (attr = attrs.get(i)); ++i) {
+                    if (attr.get_attr_type() === IBus.AttrType.BACKGROUND) {
+                        let start = attr.get_start_index();
+                        end = attr.get_end_index();
+                        if (pos !== start)
+                            console.log('Fake Highlight!!!!!');
+                        break;
+                    }
+                }
+
+                // mutter bug https://gitlab.gnome.org/GNOME/mutter/-/issues/3547
+                let endb = this._encoder.encode(s.slice(0, end)).length;
+
+                if (visible)
+                    Main.inputMethod.set_preedit_text(s, pos, endb, mode, true);
+                else if (this._preeditVisible)
+                    Main.inputMethod.set_preedit_text(null, pos, endb, mode, true);
+
+                this._preeditVisible = visible;
+            }
+        );
+    }
+}
